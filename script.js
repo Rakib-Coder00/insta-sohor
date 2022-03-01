62cd2197cb44c0614ca799e38f9b8d0f2c345436
Rakib-Dev <rhbappy456@gmail.com>
2022-03-01 10:20:41 +0600

    setup project

---
+++ script.js
@@ -0,0 +1,9 @@
+const  apiUrl = ''
+
+const imgPath = ''
+
+const searchApi = ''
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
